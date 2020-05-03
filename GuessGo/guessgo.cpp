10ba5f260e926e2f2aa8be267a3c1760e62f81af
najiasa <najiasa@users.noreply.github.com>
2020-05-03 16:27:08 +0800

    Added the definition of function start_options() and deleted a line in print_rules()

--- GuessGo/guessgo.cpp
+++ GuessGo/guessgo.cpp
@@ -10,7 +10,68 @@ bool previous_game();
 //function to restore the saved game
 void restore_game();
 
-//function to print print_rules
+//function to take input for the size of grid and pool
+void get_input();
+
+// function to randomly select words
+void select_words();
+
+//function to let user pick words for their grid
+void pick_words();
+
+//function to check if the picks are valid
+void check_words();
+
+//function to select who guesses first
+void do_toss();
+
+// function to verify the computer and user's guesses
+void check_guess();
+
+// function to cross out guessed words
+void call_out();
+
+//function to check for the winner
+void get_winner();
+
+//function to save game for later
+void save_game();
+
+//function to control game flow
+void play_game();
+
+//function for options at the start of games
+void start_options(){
+  int answer;
+
+  cout << "To view the rules, press 1" << endl;
+  cout << "To start a new game, press 2" <<endl;
+
+  if (previous_game){
+    cout << "To continue the previous game, press 3" << endl;
+  }
+
+    cout << '\n';
+    cin >> answer;
+
+    if (answer == 1){
+      cout << '\n';
+      print_rules(); }
+
+    else if (answer == 2){
+      play_game();
+    }
+
+    else if (answer==3){
+      restore_game();
+      return; }
+
+    else{
+      cout << "Invalid input. Please try again." << endl;
+      cin >> answer; }
+}
+
+//function to print an introduction to the game
 void print_rules(){
   char ans;
   bool keeplooping = true;
@@ -48,42 +109,8 @@ void print_rules(){
   cout << '\n' <<endl;
 
   fin.close();
-  cout << "you've come here" <<endl;
   start_options();
 }
-
-//function for options at the start of games
-void start_options();
-
-//function to take input for the size of grid and pool
-void get_input();
-
-// function to randomly select words
-void select_words();
-
-//function to let user pick words for their grid
-void pick_words();
-
-//function to check if the picks are valid
-void check_words();
-
-//function to select who guesses first
-void do_toss();
-
-// function to verify the computer and user's guesses
-void check_guess();
-
-// function to cross out guessed words
-void call_out();
-
-//function to check for the winner
-void get_winner();
-
-//function to save game for later
-void save_game();
-
-//function to control game flow
-void play_game();
 
 int main(){
   return 0;
