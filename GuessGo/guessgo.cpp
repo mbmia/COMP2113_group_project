3145e6bf256dff426adf8967908e13c0757d6816
najiasa <najiasa@users.noreply.github.com>
2020-05-03 16:19:30 +0800

    Added function declarations

---
+++ GuessGo/guessgo.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+
+//function to check if there are any saved games
+bool previous_game();
+
+//function to restore the saved game
+void restore_game();
+
+//function to print print_rules
+void print_rules();
+
+//function for options at the start of games
+void start_options();
+
+//function to take input for the size of grid and pool
+void get_input();
+
+// function to randomly select words
+void select_words();
+
+//function to let user pick words for their grid
+void pick_words();
+
+//function to check if the picks are valid
+void check_words();
+
+//function to select who guesses first
+void do_toss();
+
+// function to verify the computer and user's guesses
+void check_guess();
+
+// function to cross out guessed words
+void call_out();
+
+//function to check for the winner
+void get_winner();
+
+//function to save game for later
+void save_game();
+
+//function to control game flow
+void play_game();
+
+int main(){
+  return 0;
+}
