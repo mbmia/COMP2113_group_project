65b547876fdebffd978bff896165ef77d4a2c110
najiasa <najiasa@users.noreply.github.com>
2020-05-04 02:09:46 +0800

    added definition of function get_input()

--- GuessGo/guessgo.cpp
+++ GuessGo/guessgo.cpp
@@ -11,7 +11,53 @@ bool previous_game();
 void restore_game();
 
 //function to take input for the size of grid and pool
-void get_input();
+void get_input(int& grid_size, int& pool_size){
+  char reply;
+  bool looper = false;
+
+  cout << "In GuessGo, your virtual grid will have the same number of rows and columns" <<endl;
+  cout<< "How many rows/columns would you like your grid to have? (It must be between 3 and 15!)"<<endl;
+  cin >> grid_size;
+
+  while (grid_size<3 || grid_size>15){
+    cout << "Your choice is out of range. Please try again." <<endl;
+    cin >> grid_size;
+  }
+  cout <<endl;
+
+  int nsquared = grid_size*grid_size;
+
+  cout << "Do you have a preference for the number of words you'll play with? (Y/N)" <<endl;
+  cin >> reply;
+
+  while (!looper){
+    if ( reply == 'Y'){
+      cout << "How many words would you like to be able to choose from? You can choose any number between "
+           <<nsquared <<" and 500." <<endl;
+      cin >> pool_size;
+
+      while (pool_size<grid_size*grid_size || pool_size>500){
+        cout << "Your choice is out of range. Please choose a number between "<<nsquared <<" and 500" <<endl;
+        cin >> pool_size;
+      }
+      looper = true;
+    }
+
+    else if (reply == 'N'){
+      cout << "Proceeding with the default pool size..." <<endl;
+
+      if (grid_size<=10)
+        pool_size = 100;
+      else if (grid_size>10 && grid_size <= 15)
+        pool_size = 225;
+
+      looper = true;
+    }
+
+    else
+      cout << "Invalid input, please try again" <<endl;
+  }
+}
 
 // function to randomly select words
 void select_words();
