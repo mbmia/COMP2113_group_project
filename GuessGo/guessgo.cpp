e2bcdcc2d7526d0a5c8dd2b3508c280c13d60afa
najiasa <najiasa@users.noreply.github.com>
2020-05-07 22:34:14 +0800

    Added function pick_computer_words()

--- GuessGo/guessgo.cpp
+++ GuessGo/guessgo.cpp
@@ -171,6 +171,42 @@ void pick_user_words(int grid_size, string** &picked_words, vector<string> word_
   }
 }
 
+void pick_computer_words(int grid_size, int pool_size, string** &computer_wordlist, vector<string> word_pool){
+  int r  = 0;
+  vector<int> p;
+
+  srand(time(NULL));
+  for (int i = 0; i<grid_size*grid_size; i++){
+    r = rand()%pool_size;
+
+    if (i==0){
+      p.push_back(r); }
+
+    else{
+      if(find(p.begin(), p.end(), r) != p.end()) {
+        i--;
+        continue;
+      }
+      else {
+        p.push_back(r);
+       }
+    }
+  }
+
+  computer_wordlist = new string*[grid_size];
+  for (int k=0; k<grid_size; k++){
+    computer_wordlist[k] = new string[grid_size];
+  }
+
+  int z = 0;
+  for (int x = 0; x<grid_size; x++){
+    for (int y=0; y<grid_size; y++){
+      computer_wordlist[x][y]=word_pool[p[z]];
+      z++;
+    }
+  }
+}
+
 
 //function to select who guesses first
 bool do_toss(){
