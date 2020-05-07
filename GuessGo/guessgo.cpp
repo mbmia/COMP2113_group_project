d850eaa41a8c2ce94b92878db72b370a03427cde
najiasa <najiasa@users.noreply.github.com>
2020-05-07 23:08:09 +0800

    Added function get_user_guess()

--- GuessGo/guessgo.cpp
+++ GuessGo/guessgo.cpp
@@ -263,7 +263,38 @@ bool do_toss(){
 }
 
 //function to get guesses from the user
-void get_user_guess();
+//checks if the guess exists in the pool
+string get_user_guess(vector<string> pool){
+    string guess, response;
+    cout << "Please type in your guess: ";
+    bool guessOk = false;
+    while (!guessOk){
+      cin >> guess;
+      if (check_words(guess, pool)==true){
+        guessOk = true;
+      }
+      else{
+        cout << "The word does not exist in the pool." <<endl;
+        cout << "Do you want to view the pool again? (Y/N)" <<endl;
+
+        cin >> response;
+
+        while(tolower(response.at(0))!='y' && tolower(response.at(0))!='n'){
+          cout << "Invalid response. (Y/N)"<<endl;
+          cin >> response;
+          }
+
+          if (tolower(response.at(0))=='y'){
+            show_pool(pool);
+            cout << "Please guess again." <<endl;}
+
+          else if (tolower(response.at(0))=='n'){
+            cout<< "Please guess again" <<endl;
+          }
+        }
+      }
+  return guess;
+}
 
 //function to get computer's guess
 void get_computer_guess();
